eed47d74f0268ce74b892a662dc69b8b51b10190
ziza <gdiaconu093@gmail.com>
2025-10-04 07:09:00 -0400

    wrote the function to copy the contents of a certain sheet from the file, but without formatting (work in progress)

--- main.py
+++ main.py
@@ -1,37 +1,88 @@
 import urllib.request
 import requests
+import openpyxl
 
-#this is the website from which I download the file (spreadsheet)
-site_url = "https://ac.tuiasi.ro/studenti/didactic/orar/"
 
-#the document link, but calculated later
-doc_url = ""
+def get_file():
+    #this is the website from which I download the file (spreadsheet)
+    site_url = "https://ac.tuiasi.ro/studenti/didactic/orar/"
 
-#try to reach the website
-site_response = urllib.request.urlopen(site_url)
-#extract its content
-site_content = site_response.read()
+    #the document link, but calculated later
+    doc_url = ""
 
-#this is the phrase the help me extract the spreadsheet
-phrase = "orar ac"
+    #try to reach the website
+    site_response = urllib.request.urlopen(site_url)
+    #extract its content
+    site_content = site_response.read()
 
-#iterate through every line of code from the website untill I reach the line which has my spreadsheet link and extract the link
-for line in site_content.decode("utf-8").splitlines():
-    if phrase in line.lower():
-        doc_url = line.split("\"")
+    #this is the phrase the help me extract the spreadsheet
+    phrase = "orar ac"
 
-        for elem in doc_url:
-            if "https" in elem:
-                doc_url = elem
-                break
-        break
+    #iterate through every line of code from the website untill I reach the line which has my spreadsheet link and extract the link
+    for line in site_content.decode("utf-8").splitlines():
+        if phrase in line.lower():
+            doc_url = line.split("\"")
 
-#extract from the url the spreadsheet id and complete it with the file format extension to export it
-doc_url = doc_url.split("/edit")[0]+"/export?format=xlsx"
+            for elem in doc_url:
+                if "https" in elem:
+                    doc_url = elem
+                    break
+            break
 
-#make a get request to the google sheet
-response = requests.get(doc_url)
+    #extract from the url the spreadsheet id and complete it with the file format extension to export it
+    doc_url = doc_url.split("/edit")[0]+"/export?format=xlsx"
 
-#write its contents to an local file
-with open("orar.xlsx", "wb") as f:
-    f.write(response.content)
+    #make a get request to the google sheet
+    response = requests.get(doc_url)
+
+    #write its contents to an local file
+    with open("orar_full.xlsx", "wb") as f:
+        f.write(response.content)
+
+
+def extract_table():
+    
+    wb_general = openpyxl.load_workbook(filename="orar_full.xlsx")
+
+
+    sheet_name = ""
+
+    print(f"Choose an year and specialization from the list: {wb_general.sheetnames}")
+    sheet_name = input()
+
+    while(sheet_name not in wb_general.sheetnames):
+        print("Doesnt fit any of the ones in the list! Please write one from the list!")
+        print(wb_general.sheetnames)
+        sheet_name = input()
+
+
+    ws_source = wb_general.worksheets[wb_general.sheetnames.index(sheet_name)]
+
+
+    #create the workbook
+    wb_personal = openpyxl.Workbook()
+
+    #select the only sheet in the workbook
+    ws_personal = wb_personal.active
+  
+    #get the dimensions of the 'table' from the source file for copying
+    mr = ws_source.max_row
+    mc = ws_source.max_column
+
+    #go through every cell in the table and copy it
+    for i in range(1, mr+1):
+        for j in range(1, mc+1):
+            c = ws_source.cell(i, j)
+            ws_personal.cell(i,j).value = c.value 
+
+
+    #save to file
+    wb_personal.save("table.xlsx")
+
+
+def main():
+    get_file()
+    extract_table()
+
+if __name__ == '__main__':
+    main()
